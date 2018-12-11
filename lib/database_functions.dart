3d005520774fe5f9313151aa16091f1b662c33b9
“earyzhe” <earyzhe@gmail.com>
2018-12-11 17:31:36 +0000

    Phase two
    Implement the feeds

--- lib/database_functions.dart
+++ lib/database_functions.dart
@@ -10,6 +10,7 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
+import 'data/strings.dart';
 
 class DatabaseFunctions {
 
@@ -424,6 +425,55 @@ class DatabaseFunctions {
     // An error happened.
       print(error);
     });
+  }
+
+  static Future<Widget> getFeed(String _listDatabaseId, bool isProfileFeed ,Function(Profile)  _dealWithProfileSelection){
+
+    StreamBuilder(
+      stream: Firestore.instance.collection(_listDatabaseId).snapshots(),
+      initialData: 10,
+      builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none: return const Center(child: Text('No internet connection'));
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      if (!snapshot.hasData) {
+                        return const Center(child: Text('Loading'));
+                      } else if (snapshot.hasError) {
+                        return const Center(child: Text('Error'));
+                      } else if (snapshot.data.documents.length < 1) {
+                        return const Center(child: Text('No data'));
+                      } else {
+                        return new Container(
+                            height: 200.0,
+                            width: 150.0,
+                            child: new FutureBuilder(
+                                future: Functions.buildProfileCardArray(context, snapshot, _listDatabaseId, _dealWithProfileSelection),
+                                builder: (BuildContext context, AsyncSnapshot futureSnapshot) {
+                                  switch (futureSnapshot.connectionState) {
+                                    case ConnectionState.none: return Text('Press button to start.');  
+                                    case ConnectionState.active:
+                                    case ConnectionState.waiting: return Center(child: Text(StringLabels.loading));                                     
+                                    case ConnectionState.done:
+                                      if (futureSnapshot.hasError){return Text('Error: ${futureSnapshot.error}');}
+                                      else if (futureSnapshot.data == null){ return Center(child: Text(StringLabels.noData));}
+                                      else{
+                                      List<dynamic> list = futureSnapshot.data;
+                                      List<dynamic> reversedlist = list.reversed.toList();
+                                      return 
+                                      ListView.builder(
+                                          itemExtent: 100,
+                                          itemCount: reversedlist.length,
+                                          itemBuilder: (BuildContext context, int index) =>
+                                              reversedlist[index]);}
+                                  }
+                                  return null; // unreachable
+                                }));
+                      }
+                  }
+                }
+      );
   }
 }
 
