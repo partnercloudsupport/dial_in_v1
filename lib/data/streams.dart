93719ebfac269c720927261d58c16f17954c8f5b
“earyzhe” <earyzhe@gmail.com>
2018-12-18 21:35:22 +0000

    Holy fuck it's streaming :)

--- lib/data/streams.dart
+++ lib/data/streams.dart
@@ -15,52 +15,54 @@ class FeedBloc{
   String get databaseId => _databaseId;
 
   final _outgoingController = BehaviorSubject<List<Profile>>();
+  final _incomingController = StreamController<QuerySnapshot>.broadcast();
+
   
   Stream<List<Profile>> get profiles => _outgoingController.stream;
+    
   
-  List<Profile> _profiles;
-
-  void add(Profile profile){
-    _getProfiles().then((_){
-      _outgoingController.add(_profiles);
-    });
-  }
-
   /// Init of the class
   FeedBloc(this._databaseId){
-    _profiles = new List();
-
-    _getProfiles().then((_){
-      _outgoingController.add(_profiles);
-    });
+   
+   _getProfiles();
+    
+    
   }
 
-  Future _getProfiles()async{
+  Future _getProfiles({Profile profile})async{
 
     String userID = await DatabaseFunctions.getCurrentUserId();
 
-    await convertStreamToListOfProfiles(Firestore.instance.collection(_databaseId)
+    _incomingController.addStream(Firestore.instance.collection(_databaseId)
     .where(DatabaseIds.user, isEqualTo: userID).snapshots());
-    
-    return;
+
+    _incomingController.stream.listen((p){
+      convertStreamToListOfProfiles(p)
+      .then((profiles){ 
+
+        if (profile != null){profiles.add(profile);}
+        _outgoingController.add(profiles);});
+      }
+    );
   }
 
-  Future convertStreamToListOfProfiles(Stream<QuerySnapshot> stream) async {
-    List<Profile> profiles;
 
-    await for (var value in stream) {
+  Future<List<Profile>> convertStreamToListOfProfiles(QuerySnapshot stream) async {
 
-      final futureProfiles = value.documents.map((doc) => 
-          DatabaseFunctions.createProfileFromDocumentSnapshot(_databaseId, doc));
-
-      profiles = await Future.wait(futureProfiles);
-      _profiles = profiles;
-      return;
-    }
+    final futureProfiles = stream.documents.map((doc) => 
+        DatabaseFunctions.createProfileFromDocumentSnapshot(_databaseId, doc));
+           
+    return await Future.wait(futureProfiles);
   }
-}
+
+
+  void add(Profile profile){
+    // _getProfiles(profile: profile);
+  }
+}  
 
 /// Social card
+
 class SocialFeedBloc{
 
   ///Other Variables
@@ -82,7 +84,6 @@ class SocialFeedBloc{
 
   Future _getProfiles()async{
   
-
     if(_databaseId == DatabaseIds.community)
     {await convertStreamToListOfProfiles(Firestore.instance.collection(DatabaseIds.recipe)
     .where(DatabaseIds.public, isEqualTo: true).snapshots());}
@@ -107,12 +108,63 @@ class SocialFeedBloc{
     }
   }
 
-  Future convertProfilesToListOfFeedProfiles(List<Profile> stream) async {
-    
-    for(var doc in stream){  /// <<<<==== changed line
-            FeedProfileData result = await Functions.createFeedProfileFromProfile(doc);
-            _profiles.add(result);
+    Future convertProfilesToListOfFeedProfiles(List<Profile> stream) async {
+      
+      for(var doc in stream){  /// <<<<==== changed line
+              FeedProfileData result = await Functions.createFeedProfileFromProfile(doc);
+              _profiles.add(result);
+      }
+      return;
     }
-    return;
-  }
+
 }
+
+//  class FeedBloc{
+
+//     ///Other Variables
+//     final String _databaseId;
+//     String get databaseId => _databaseId;
+
+//     final _outgoingController = BehaviorSubject<List<Profile>>();
+    
+//     Stream<List<Profile>> get profiles => _outgoingController.stream;
+    
+//     List<Profile> _profiles = new List<Profile>();
+
+//     void add(Profile profile){
+//       _profiles.add(profile);
+//       _outgoingController.add(_profiles);
+//     }
+
+//     /// Init of the class
+//     FeedBloc(this._databaseId){
+      
+//       _getProfiles().then((_){
+//         _outgoingController.add(_profiles);
+//       });
+//     }
+
+//     Future _getProfiles()async{
+
+//       String userID = await DatabaseFunctions.getCurrentUserId();
+
+//       await convertStreamToListOfProfiles(Firestore.instance.collection(_databaseId)
+//       .where(DatabaseIds.user, isEqualTo: userID).snapshots());
+      
+//       return;
+//     }
+
+    // Future convertStreamToListOfProfiles(Stream<QuerySnapshot> stream) async {
+    //   List<Profile> profiles;
+
+    //   await for (var value in stream) {
+
+    //     final futureProfiles = value.documents.map((doc) => 
+    //         DatabaseFunctions.createProfileFromDocumentSnapshot(_databaseId, doc));
+
+    //     profiles = await Future.wait(futureProfiles);
+    //     _profiles = profiles;
+    //     return;
+    //   }
+    // }
+//   }
